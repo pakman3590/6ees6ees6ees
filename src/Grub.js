5e340048d31e3e61627138347dc5a633951e3b96
Matthew Pak <mattpak@gmail.com>
2022-02-28 11:51:51 -0800

    Passed grub tests

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,11 @@
 class Grub {
-};
+  constructor() {
+    // set age, color, food properties
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+  eat() {
+    return 'Mmmmmmmmm jelly';
+  }
+}
