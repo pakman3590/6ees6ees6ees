39914b648e46a373eb2b5db01c815c592f725d8e
Matthew Pak <mattpak@gmail.com>
2022-02-28 11:57:54 -0800

    Passed bee tests

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
-class Bee {
-  // TODO..
-};
+class Bee extends Grub {
+  constructor() {
+    // reference superclass
+    super();
+    // reassign age, color properties
+    this.age = 5;
+    this.color = 'yellow';
+    // new job property
+    this.job = 'Keep on growing';
+  }
+}
