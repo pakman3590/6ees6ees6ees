6a2e023e015f32a1498ee324185fc52f146bb412
Matthew Pak <mattpak@gmail.com>
2022-02-28 12:08:17 -0800

    Passed ForagerBee and RetiredForagerBee tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,17 @@
-class ForagerBee {
-  // TODO..
-};
+// reference bee superclass
+class ForagerBee extends Bee {
+  constructor() {
+    // reference super
+    super();
+    // update age, job properties
+    this.age = 10;
+    this.job = 'find pollen';
+    // new canFly and treasureChest properties
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  // new forage method
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
+}
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,18 @@
-class RetiredForagerBee {
-  // TODO..
-};
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+    super();
+    // update age, job, canFly, and color properties
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+  }
+  // update forage method
+  forage() {
+    return 'I am too old, let me play cards instead';
+  }
+  // new gamble method
+  gamble(treasure) {
+    this.treasureChest.push(treasure);
+  }
+}
