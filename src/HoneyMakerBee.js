3153df0dfc16d48d60880749b04741eadb56fb88
Matthew Pak <mattpak@gmail.com>
2022-02-28 12:01:36 -0800

    Passed HoneyMakerBee tests

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
-class HoneyMakerBee {
-  // TODO..
-};
+// reference bee superclass
+class HoneyMakerBee extends Bee {
+  // constructor
+  constructor() {
+    // reference super
+    super();
+    // update age, job properties
+    this.age = 10;
+    this.job = 'make honey';
+    // new honeypot property
+    this.honeyPot = 0;
+  }
+  // new makeHoney method
+  makeHoney() {
+    this.honeyPot ++;
+  }
+  // new giveHoney method
+  giveHoney() {
+    this.honeyPot --;
+  }
+}
